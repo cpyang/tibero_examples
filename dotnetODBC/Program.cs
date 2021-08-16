3066dbf7112435ebf43f6d90171e4174eb982a6b
Conrad Yang <cpyang@gmail.com>
2021-08-17 02:02:24 +0800

    dotnet ODBC with C#

---
+++ dotnetODBC/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.Odbc;
+ 
+namespace test {
+    class Program {
+        static void Main(string[] args) {
+	    bool tibero = true;
+	    //tibero = false;
+	    OdbcConnectionStringBuilder builder;
+
+	    if (tibero) {
+		builder = new OdbcConnectionStringBuilder {
+		    Driver = "Tibero"
+		};
+		builder.Add("DSN", "tibero");
+		builder.Add("HostName", "localhost");
+		builder.Add("PortNumber", "8629");
+		builder.Add("Database", "tibero");
+	    } else {
+		builder = new OdbcConnectionStringBuilder {
+		    Driver = "Oracle"
+		};
+		builder.Add("DSN", "oracle");
+		builder.Add("HostName", "localhost");
+		builder.Add("PortNumber", "1521");
+		builder.Add("Database", "XEPDB1");
+	    }
+ 
+	    string connection_string = builder.ConnectionString;
+            using (OdbcConnection connection = new OdbcConnection(connection_string))
+            {
+                string sqlQuery = "SELECT 'Hello ' as hello, 'World!' as world FROM dual UNION SELECT '哈囉', '世界!' FROM dual";
+		OdbcCommand command;
+		OdbcDataReader reader;
+                try {
+	            Console.WriteLine("Connecting to \"{0}\"", connection_string);
+                    connection.Open();
+                    command = new OdbcCommand(sqlQuery, connection);
+		    Console.WriteLine("ExecuteReader()");
+                    reader = command.ExecuteReader();
+
+                    for (int i=0; i<reader.FieldCount; i++) {
+                        Console.Write("Column({0}) {1}\t", i, reader.GetName(i));
+                    }
+                    Console.Write("\n");
+ 
+                    if(reader.HasRows) {
+                        while(reader.Read()) {
+                            Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+			}
+                    }
+                    reader.Close();
+                    command.Dispose();
+		} catch (OdbcException e) {
+                    for (int i=0; i < e.Errors.Count; i++) {
+                        Console.Write("Exception #{0}\nMessage:{1}\nNative:{2}\nSource:{3}\nSQL:{4}\nStackTrace:{5}\n",
+                            i, e.Errors[i].Message, e.Errors[i].NativeError.ToString(),
+			    e.Errors[i].Source, e.Errors[i].SQLState, e.StackTrace.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ dotnetODBC/README.md
@@ -0,0 +1,20 @@
+# Sample Code for dotnet ODBC in C#
+    #dotnet new console
+    #dotnet add package System.Data.Odbc
+    . ./setenv.sh
+    dotnet build
+    dotnet run
+
+#Required environment variables:  
+## Enable Unicode Support
+export TB_NLS_LANG=UTF8  
+export TBCLI_WCHAR_TYPE=UCS2  
+
+#Optional environment variables:  
+## Set odbc.ini and tbdsn.tbr location
+export TB_DSN_FILE=`pwd`/tbdsn.tbr  
+export ODBCINI=`pwd`/odbc.ini  
+
+## Enable Trace - Useful when testing connection with isql and iusql
+export TOK_TBCLI_LOG_LVL=trace  
+export TOK_TBCLI_LOG_DIR=`pwd`  
