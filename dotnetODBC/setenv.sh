b42cb4d94f8772a02a351c1bec2233dba582303a
Conrad Yang <cpyang@gmail.com>
2021-09-15 11:01:47 +0800

    Tibero ODBC client trace setting

--- dotnetODBC/setenv.sh
+++ dotnetODBC/setenv.sh
@@ -2,5 +2,5 @@ export TB_NLS_LANG=UTF8
 export TBCLI_WCHAR_TYPE=UCS2
 export TB_DSN_FILE=`pwd`/tbdsn.tbr
 export ODBCINI=`pwd`/odbc.ini
-export TOK_TBCLI_LOG_LVL=trace
-export TOK_TBCLI_LOG_DIR=`pwd`
+#export TBCLI_LOG_LVL=trace
+#export TBCLI_LOG_DIR=`pwd`
